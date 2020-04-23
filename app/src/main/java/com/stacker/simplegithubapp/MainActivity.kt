393b8094e0640107811af8b9ebaaf75fa4639d70
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2020-04-24 00:11:40 +0900

    Following, Search 액티비티 생성

--- app/src/main/java/com/stacker/simplegithubapp/MainActivity.kt
+++ app/src/main/java/com/stacker/simplegithubapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.stacker.simplegithubapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<View>(R.id.BottomNavigationBar)
+
+
     }
 }
+
+
---
+++ app/src/main/java/com/stacker/simplegithubapp/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.stacker.simplegithubapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
