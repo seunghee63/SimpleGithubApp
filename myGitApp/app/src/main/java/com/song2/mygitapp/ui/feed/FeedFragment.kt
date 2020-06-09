a53597ebe192403af6ecd4ce2f701e190ea5b76e
song <0603yang@naver.com>
2020-06-10 06:01:04 +0900

    [Add] feedTab - feed list ui 추가

--- myGitApp/app/src/main/java/com/song2/mygitapp/ui/feed/FeedFragment.kt
+++ myGitApp/app/src/main/java/com/song2/mygitapp/ui/feed/FeedFragment.kt
@@ -2,20 +2,43 @@ package com.song2.mygitapp.ui.feed
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.song2.mygitapp.R
 import com.song2.mygitapp.data.GetFeedResponse
 import com.song2.mygitapp.network.ApiClient
+import com.song2.mygitapp.ui.star.StarListAdapter
+import com.song2.mygitapp.ui.star.StarRepo
+import kotlinx.android.synthetic.main.fragment_feed.*
+import kotlinx.android.synthetic.main.fragment_star.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class FeedFragment : Fragment(R.layout.fragment_feed) {
+class FeedFragment : Fragment() {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerView()
         getFeedResponse("seunghee63")
+    }
+
+    private fun setRecyclerView(){
+        val adapter = StarListAdapter()
+        adapter.submitList(listOf(StarRepo("0", "seunghee63","FlowerRecognition","This project identify some kind of flowers and developed by Android users.", " Java", 3,"16 Jan"),
+            StarRepo("0", "seunghee63","FlowerRecognition","This project identify some kind of flowers and developed by Android users.", " Java", 3,"16 Jan"),
+            StarRepo("0", "seunghee63","FlowerRecognition","This project identify some kind of flowers and developed by Android users.", " Java", 3,"16 Jan"),
+            StarRepo("0", "seunghee63","FlowerRecognition","This project identify some kind of flowers and developed by Android users.", " Java", 3,"16 Jan")
+        ))
+        rv_feed_list.layoutManager = LinearLayoutManager(activity)
+        rv_feed_list.adapter = adapter
     }
 
     private fun getFeedResponse(id : String?) {
