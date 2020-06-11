770c459163177a67e1d38689f8df0fa6d045fe27
song <0603yang@naver.com>
2020-06-11 10:32:57 +0900

    (작업중) recyclerview에 databinding 적용

--- myGitApp/app/src/main/java/com/song2/mygitapp/util/BindingAdapter.kt
+++ myGitApp/app/src/main/java/com/song2/mygitapp/util/BindingAdapter.kt
@@ -4,16 +4,27 @@ import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.song2.mygitapp.ui.star.StarListAdapter
+import com.song2.mygitapp.ui.star.StarRepo
 
 @BindingAdapter("app:background_grade")
 fun setGrade(view: View, grade: Int) {
     view.setBackgroundColor(Color.argb(((grade*100)),200, 200, 200))
 }
 
-@BindingAdapter("app:setImage")
+@BindingAdapter("app:set_image")
 fun setImageUrl(view: ImageView, profile: String) {
     Glide.with(view.context)
         .load(profile)
         .into(view)
+}
+
+@BindingAdapter("app:set_data")
+fun setData(view: RecyclerView, openDatas: List<StarRepo>?) {
+
+    (view.adapter as StarListAdapter).run {
+        submitList(openDatas)
+    }
 }
