de72aad74cec8a4711252d8dc5e8f98480c75da0
song <0603yang@naver.com>
2020-06-11 10:15:34 +0900

    [Modify] bindingAdapter 이름변경

--- myGitApp/app/src/main/java/com/song2/mygitapp/util/BindingAdapter.kt
+++ myGitApp/app/src/main/java/com/song2/mygitapp/util/BindingAdapter.kt
@@ -6,12 +6,12 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("android:background_grade")
+@BindingAdapter("app:background_grade")
 fun setGrade(view: View, grade: Int) {
     view.setBackgroundColor(Color.argb(((grade*100)),200, 200, 200))
 }
 
-@BindingAdapter("android:setImage")
+@BindingAdapter("app:setImage")
 fun setImageUrl(view: ImageView, profile: String) {
     Glide.with(view.context)
         .load(profile)
